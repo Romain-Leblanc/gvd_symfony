c66e6d56d9e62bf26feddacb0d675422fea05b1e
Romain-Leblanc <romleb2001@gmail.com>
2022-12-02 23:38:21 +0100

    ajout formulaire facture sans vérification logique

--- src/Controller/FactureController.php
+++ src/Controller/FactureController.php
@@ -2,8 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Facture;
+use App\Form\AjoutFactureType;
+use App\Repository\EtatRepository;
 use App\Repository\FactureRepository;
+use App\Repository\InterventionRepository;
+use App\Repository\TVARepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,5 +26,89 @@ class FactureController extends AbstractController
         return $this->render('facture/index.html.twig', [
             'lesFactures' => $lesFactures
         ]);
+    }
+
+    /**
+     * @Route("/facture/ajouter", name="facture_ajouter")
+     */
+    public function ajouter(FactureRepository $factureRepository, InterventionRepository $interventionRepository, TVARepository $TVARepository, EtatRepository $etatRepository, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // Retourne la liste des interventions qui sont terminées
+        $listeInterventions = $interventionRepository->findBy(['fk_etat' => $etatRepository->findOneBy(['etat' => 'Terminé'])->getId()]);
+
+        // Si aucune intervention est terminée (et donc n'a pas besoin d'être facturé), alors on renvoie un message puis une redirection
+        if(empty($listeInterventions)){
+            echo '<script type="application/javascript">alert("Aucun intervention à facturée");</script>';
+            $this->redirectToRoute("facture_index");
+        }
+
+        // Retourne le taux de TVA égal à 20
+        $tauxTVA = $TVARepository->findOneBy(["taux" => 20])->getTaux();
+
+        // Si aucune intervention est terminée (et donc n'a pas besoin d'être facturé), alors on renvoie un message puis une redirection
+/*        if(empty($tauxTVA)){
+            echo '<script type="application/javascript">alert("Aucun taux de TVA égal à 20% est présent dans la base de données.");</script>';
+            $this->redirectToRoute("facture_index");
+        }*/
+
+        // Création de l'objet Facture(), génération du formulaire d'ajout d'une Facture avec l'objet Facture et manipulation des données de l'objet Request
+        $uneFacture = new Facture();
+        $form = $this->createForm(AjoutFactureType::class, $uneFacture);
+        $form->handleRequest($request);
+//        dd($uneFacture);
+
+        // Si le formulaire a bien été soumis et est validé
+        if ($form->isSubmitted() && $form->isValid()){
+            // On persiste l'objet Facture dans l'entité Facture
+            $entityManager->persist($uneFacture);
+            $entityManager->flush();
+
+            // Equivalent de la fonction lastInsertId() qui permet de récupérer le dernier identifiant insérée dans la table facture
+            $idFacture = $uneFacture->getId();
+
+            // Récupère la liste des interventions terminées du client qui ne sont pas facturées
+            $liste = $interventionRepository->findBy(['FK_Client' => $uneFacture->getFKClient()->getId(), 'FK_Facture' => null, 'FK_Etat' => $etatRepository->findOneBy(['Etat' => 'Terminé'])->getId()]);
+
+            // Boucle sur chaque intervention pour récupérer l'identifiant de l'intervention du client à facturé puis concatène ces identifiants dans un tableau
+            $tabIdInterventions = [];
+            foreach ($liste as $key => $value){
+                array_push($tabIdInterventions, $value->getId());
+            }
+            // Met à jour l'etat des interventions à 'Facturé' et associe le dernier n° facture aux identifiants du tableau ci-dessus
+            $interventionRepository->updateInterventionByEtatAndNumFacture($tabIdInterventions, $etatRepository->findOneBy(['Etat' => 'Facturé'])->getId(), $idFacture);
+
+            // Redirection de la page vers la route "facture_index"
+            return $this->redirectToRoute('facture_index');
+        }
+
+        return $this->render('facture/ajout.html.twig', [
+            'errors' => $form->getErrors(true),
+            'formAjoutFacture' => $form->createView(),
+            'listeInterventions' => $listeInterventions,
+            'tauxTVA' => $tauxTVA,
+        ]);
+    }
+
+    /**
+     * @Route("/facture/infos", name="facture_infos")
+     */
+    public function infos(InterventionRepository $interventionRepository, EtatRepository $etatRepository, Request $request)
+    {
+        $id = (int) $request->request->get('clientID');
+        // Si la requête est bien en POST
+        if($request->isMethod(Request::METHOD_POST)) {
+            if (!empty($id) && $id !== 0) {
+                // Renvoi la liste des interventions non facturés des véhicules du client
+                $liste = $interventionRepository->findBy(['fk_client' => $id, 'fk_facture' => null, 'fk_etat' => $etatRepository->findOneBy(['etat' => 'Terminé'])->getId()]);
+                die(var_dump($liste));
+                return $this->json(['donnees' => $liste]);
+            }
+            else {
+                return $this->json(['donnees' => ""]);
+            }
+        }
+        else {
+            return $this->json(['donnees' => ""]);
+        }
     }
 }
--- src/Form/AjoutFactureType.php
+++ src/Form/AjoutFactureType.php
@@ -2,8 +2,18 @@
 
 namespace App\Form;
 
+use App\Entity\Client;
+use App\Entity\Etat;
 use App\Entity\Facture;
+use App\Entity\Intervention;
+use App\Entity\MoyenPaiement;
+use App\Entity\TVA;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query\Expr\Join;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,15 +21,109 @@ class AjoutFactureType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $dateFacture = new \DateTime();
+
         $builder
-            ->add('date_facture')
-            ->add('date_paiement')
-            ->add('montant_ht')
-            ->add('montant_tva')
-            ->add('montant_ttc')
-            ->add('fk_client')
-            ->add('fk_taux')
-            ->add('fk_moyen_paiement')
+            ->add('date_facture', DateType::class, [
+                'widget' => 'single_text',
+                'data' => $dateFacture,
+                'attr' => [
+                    'class' => 'form-control input-50',
+                    'min' => $dateFacture->format('Y-m-d')
+                ],
+                'label' => "Date facture :",
+                'label_attr' => [
+                    'class' => 'text-center col-md-5 col-form-label'
+                ],
+                'required' => true
+            ])
+            ->add('fk_client', EntityType::class, [
+                'class' => Client::class,
+                "placeholder" => "-- CLIENTS --",
+                // Retourne la liste des clients qui ont au moins 1 intervention terminée non facturé
+                'query_builder' => function(EntityRepository $entityRepository) {
+                    return $entityRepository->createQueryBuilder("c")
+                        ->select("c")
+                        ->innerJoin(Intervention::class, 'i', Join::WITH, 'i.fk_client = c.id')
+                        ->innerJoin(Etat::class, 'e', Join::WITH, 'i.fk_etat = e.id')
+                        ->where("e.etat = :etat")
+                        ->setParameter('etat', 'Terminé')
+                        ->andWhere("i.fk_facture IS NULL")
+                    ;
+                },
+                'choice_label' => function(Client $client){
+                    return mb_strtoupper($client->getNom())." ".ucfirst($client->getPrenom())." - ".mb_strtoupper($client->getVille());
+                },
+                'attr' => [
+                    'class' => 'form-select',
+                    'onchange' => 'getInfosFromClientFacture();'
+                ],
+                'label' => "Client :",
+                'label_attr' => [
+                    'class' => 'text-center col-md-5 col-form-label'
+                ],
+                'required' => true
+            ])
+            ->add('fk_taux', EntityType::class, [
+                'class' => TVA::class,
+                // Retourne une liste avec comme valeur le taux de TVA a 20%
+                'query_builder' => function(EntityRepository $entityRepository) {
+                    return $entityRepository->createQueryBuilder("t")
+                        ->select("t")
+                        ->where("t.taux = :taux")
+                        ->setParameter('taux', '20')
+                        ;
+                },
+                'choice_label' => function(TVA $taux){
+                    return $taux->getTaux()." %";
+                },
+                'attr' => [
+                    'class' => 'form-select input-50',
+                ],
+                'label' => "Taux :",
+                'label_attr' => [
+                    'class' => 'text-center col-md-5 col-form-label'
+                ],
+                'required' => true
+            ])
+            ->add('fk_moyen_paiement', EntityType::class, [
+                'class' => MoyenPaiement::class,
+                'choice_label' => function(MoyenPaiement $moyenPaiement){
+                    return $moyenPaiement->getMoyenPaiement();
+                },
+                'attr' => [
+                    'class' => 'form-select',
+                    'disabled' => true,
+                ],
+                'label' => "Moyen paiement :",
+                'label_attr' => [
+                    'class' => 'text-center col-md-6 col-form-label'
+                ],
+                'required' => true
+            ])
+            ->add('date_paiement', DateType::class, [
+                'widget' => 'single_text',
+                'data' => $dateFacture,
+                'attr' => [
+                    'class' => 'form-control',
+                    'min' => $dateFacture->format('Y-m-d'),
+                    'disabled' => true,
+                ],
+                'label' => "Date paiement :",
+                'label_attr' => [
+                    'class' => 'text-center col-md-6 col-form-label'
+                ],
+                'required' => true
+            ])
+            ->add('montant_ht', HiddenType::class, [
+                'data' => '0,00 €'
+            ])
+            ->add('montant_tva', HiddenType::class, [
+                'data' => '0,00 €'
+            ])
+            ->add('montant_ttc', HiddenType::class, [
+                'data' => '0,00 €'
+            ])
         ;
     }
 
